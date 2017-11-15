fd4a1814211d05c052c31a45c6a85580698f27cc
Aditya Pramana <33028351+ojoakua-10bit@users.noreply.github.com>
2017-11-15 15:14:43 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -7,15 +7,16 @@
 #include <dirent.h>
 #include <errno.h>
 #include <sys/time.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
-static const char *dir = "/home/tidakumum/Documents/hehe/anu/files";
+static const char *dpath = "/home/tidakumum/Documents";
 
-static int mboh_getattr(const char *path, struct stat *stbuf)
-{
-    int res;
-	char fpath[1000];
-	sprintf(fpath,"%s%s",dirpath,path);
-	res = lstat(fpath, stbuf);
+static int anu_getattr(const char *path, struct stat *stbuf) {
+  int res;
+	char filpth[1000];
+	sprintf(filpth,"%s%s",dpath,path);
+	res = lstat(filpth, stbuf);
 
 	if (res == -1)
 		return -errno;
@@ -23,16 +24,14 @@ static int mboh_getattr(const char *path, struct stat *stbuf)
 	return 0;
 }
 
-static int mboh_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-		       off_t offset, struct fuse_file_info *fi)
-{
-    char fpath[1000];
+static int anu_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi){
+  char filpth[1000];
 	if(strcmp(path,"/") == 0)
 	{
-		path=dirpath;
-		sprintf(fpath,"%s",path);
+		path=dpath;
+		sprintf(filpth,"%s",path);
 	}
-	else sprintf(fpath, "%s%s",dirpath,path);
+	else sprintf(filpth, "%s%s",dpath,path);
 	int res = 0;
 
 	DIR *dp;
@@ -41,7 +40,7 @@ static int mboh_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 	(void) offset;
 	(void) fi;
 
-	dp = opendir(fpath);
+	dp = opendir(filpth);
 	if (dp == NULL)
 		return -errno;
 
@@ -58,40 +57,51 @@ static int mboh_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 	return 0;
 }
 
-static int mboh_read(const char *path, char *buf, size_t size, off_t offset,
-		    struct fuse_file_info *fi)
-{
-    char fpath[1000];
+static int anu_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi){
+    char filpth[1000];
 	if(strcmp(path,"/") == 0)
 	{
-		path=dirpath;
-		sprintf(fpath,"%s",path);
+		path=dpath;
+		sprintf(filpth,"%s",path);
 	}
-	else sprintf(fpath, "%s%s",dirpath,path);
+	else sprintf(filpth, "%s%s",dpath,path);
 	int res = 0;
     int fd = 0 ;
 
 	(void) fi;
-	fd = open(fpath, O_RDONLY);
-	if (fd == -1)
-		return -errno;
+	if (strcmp(filpth+(strlen(filpth)-4), ".pdf") == 0 || strcmp(filpth+(strlen(filpth)-4), ".txt") == 0 || strcmp(filpth+(strlen(filpth)-4), ".doc") == 0){
+        fprintf(stderr, "Terjadi kesalahan! File berisi konten berbahaya.\n");
+        char buf[1024];
+        sprintf(buf,"%s/rahasia%s.ditandai", dpath, path);
+        if(rename(filpth, buf) != 0){
+            char buf2[1024];
+            sprintf(buf2, "%s/rahasia", dpath);
+            mkdir(buf2, 0775);
+            rename(filpth, buf);
+        }
+        chmod(buf, 0);
+    }
+    else{
+        fd = open(filpth, O_RDONLY);
+        if (fd == -1)
+            return -errno;
 
-	res = pread(fd, buf, size, offset);
-	if (res == -1)
-		res = -errno;
+        res = pread(fd, buf, size, offset);
+        if (res == -1)
+            res = -errno;
 
-	close(fd);
+        close(fd);
+    }
 	return res;
 }
 
-static struct fuse_operations mboh_oper = {
-	.getattr	= mboh_getattr,
-	.readdir	= mboh_readdir,
-	.read		= mboh_read,
+static struct fuse_operations anu_oper = {
+	.getattr	= anu_getattr,
+	.readdir	= anu_readdir,
+	.read		= anu_read,
 };
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	umask(0);
-	return fuse_main(argc, argv, &mboh_oper, NULL);
+	return fuse_main(argc, argv, &anu_oper, NULL);
 }
